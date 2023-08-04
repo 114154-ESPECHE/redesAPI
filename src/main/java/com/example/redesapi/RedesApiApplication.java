bf137e038876c75d5c7c0e52838b31a1c2cdbbc6
114154-ESPECHE <114154@tecnicatura.frc.utn.edu.ar>
2023-08-04 10:17:49 -0300

    iniciando proyecto

---
+++ src/main/java/com/example/redesapi/RedesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.redesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/redesapi/RedesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
